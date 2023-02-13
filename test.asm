c47a9853f56942f49cc184537356d5314fe4b2ea
Peter Woolsey <woolsey@student.ubc.ca>
2023-02-13 12:12:45 -0800

    pushing what i have before updating

--- test.asm
+++ test.asm
@@ -56,9 +56,30 @@ dseg at 30H
 
 ; Interrupt vectors:
 cseg
+
 org 0x0000 ; Reset vector
     ljmp MainProgram
 
+org 0x0003 ; External interrupt 0 vector (not used in this code)
+	reti
+
+org 0x000B ; Timer/Counter 0 overflow interrupt vector (not used in this code)
+	reti
+
+org 0x0013 ; External interrupt 1 vector. (not used in this code)
+	reti
+
+org 0x001B ; Timer/Counter 1 overflow interrupt vector. Used in this code to replay the wave file.
+	ljmp Timer1_ISR
+
+org 0x0023 ; Serial port receive/transmit interrupt vector (not used in this code)
+	reti
+
+org 0x005b ; Timer 2 interrupt vector. (not used in this code)
+	reti
+
+org 0x0063 ; ADC interrupt (vector must be present if debugger is used)
+	reti
 
 
 Timer1_ISR:
@@ -101,13 +122,79 @@ Timer1_ISR_Done:
 	pop acc
 	reti
 
+;---------------------------------;
+; Sends a byte via serial port    ;
+;---------------------------------;
+putchar:
+	jbc	TI,putchar_L1
+	sjmp putchar
+putchar_L1:
+	mov	SBUF,a
+	ret
 
-MainProgram:
-    mov w+2, #0x3f
-	mov w+1, #0xff
-	mov w+0, #0xff
+;---------------------------------;
+; Receive a byte from serial port ;
+;---------------------------------;
+getchar:
+	jbc	RI,getchar_L1
+	sjmp getchar
+getchar_L1:
+	mov	a,SBUF
+	ret
+
+;---------------------------------;
+; Sends AND receives a byte via   ;
+; SPI.                            ;
+;---------------------------------;
+Send_SPI:
+	SPIBIT MAC
+	    ; Send/Receive bit %0
+		rlc a
+		mov MY_MOSI, c
+		setb MY_SCLK
+		mov c, MY_MISO
+		clr MY_SCLK
+		mov acc.0, c
+	ENDMAC
 	
-    anl	TMOD, #0x0F ; Clear the bits of timer 1 in TMOD
+	SPIBIT(7)
+	SPIBIT(6)
+	SPIBIT(5)
+	SPIBIT(4)
+	SPIBIT(3)
+	SPIBIT(2)
+	SPIBIT(1)
+	SPIBIT(0)
+
+	ret
+
+;Added this before Main
+Init_all:
+    ; Since the reset button bounces, we need to wait a bit before
+    ; sending messages, otherwise we risk displaying gibberish!
+    mov R1, #222
+    mov R0, #166
+    djnz R0, $   ; 3 cycles->3*45.21123ns*166=22.51519us
+    djnz R1, $-4 ; 22.51519us*222=4.998ms
+    ; Now we can proceed with the configuration
+	
+	; Enable serial communication and set up baud rate
+	orl	PCON,#0x80
+	mov	SCON,#0x52
+	mov	BDRCON,#0x00
+	mov	BRL,#BRG_VAL
+	mov	BDRCON,#0x1E ; BDRCON=BRR|TBCK|RBCK|SPD;
+	
+	; Configure SPI pins and turn off speaker
+	anl P2M0, #0b_1100_1110
+	orl P2M1, #0b_0011_0001
+	setb MY_MISO  ; Configured as input
+	setb FLASH_CE ; CS=1 for SPI flash memory
+	clr MY_SCLK   ; Rest state of SCLK=0
+	clr SPEAKER   ; Turn off speaker.
+	
+	; Configure timer 1
+	anl	TMOD, #0x0F ; Clear the bits of timer 1 in TMOD
 	orl	TMOD, #0x10 ; Set timer 1 in 16-bit timer mode.  Don't change the bits of timer 0
 	mov TH1, #high(TIMER1_RELOAD)
 	mov TL1, #low(TIMER1_RELOAD)
@@ -125,6 +212,7 @@ MainProgram:
 	mov DADH, #0x80 ; Middle of scale
 	mov DADL, #0
 	orl DADC, #0b_0100_0000 ; Start DAC by GO/BSY=1
+check_DAC_init:
 	mov a, DADC
 	jb acc.6, check_DAC_init ; Wait for DAC to finish
 	
@@ -135,9 +223,247 @@ MainProgram:
 	; (An external R-2R produces much better quality sound)
 	mov P0M0, #0b_0000_0000
 	mov P0M1, #0b_1111_1111
+	
+	ret
 
+MainProgram:
+    mov SP, #0x7f ; Setup stack pointer to the start of indirectly accessable data memory minus one
+    lcall Init_all ; Initialize the hardware 
+
+forever_loop:
+	jb RI, serial_get
+	jb P4.5, forever_loop ; Check if push-button pressed
+	jnb P4.5, $ ; Wait for push-button release
+	; Play the whole memory
+	clr TR1 ; Stop Timer 1 ISR from playing previous request
+	setb FLASH_CE
+	clr SPEAKER ; Turn off speaker.
+	
+	clr FLASH_CE ; Enable SPI Flash
+	mov a, #READ_BYTES
+	lcall Send_SPI
+	; Set the initial position in memory where to start playing
+	mov a, #0x00
+	lcall Send_SPI
+	mov a, #0x00
+	lcall Send_SPI
+	mov a, #0x00
+	lcall Send_SPI
+	mov a, #0x00 ; Request first byte to send to DAC
+	lcall Send_SPI
+	
+	; How many bytes to play? All of them!  Asume 4Mbytes memory: 0x3fffff
+	mov w+2, #0x3f
+	mov w+1, #0xff
+	mov w+0, #0xff
+	
 	setb SPEAKER ; Turn on speaker.
 	setb TR1 ; Start playback by enabling Timer 1
-    forever_loop
-    ljmp forever_loop
+	ljmp forever_loop
+	
+serial_get:
+	lcall getchar ; Wait for data to arrive
+	cjne a, #'#', forever_loop ; Message format is #n[data] where 'n' is '0' to '9'
+	clr TR1 ; Stop Timer 1 from playing previous request
+	setb FLASH_CE ; Disable SPI Flash	
+	clr SPEAKER ; Turn off speaker.
+	lcall getchar
+
+;---------------------------------------------------------	
+	cjne a, #'0' , Command_0_skip
+Command_0_start: ; Identify command
+	clr FLASH_CE ; Enable SPI Flash	
+	mov a, #READ_DEVICE_ID
+	lcall Send_SPI	
+	mov a, #0x55
+	lcall Send_SPI
+	lcall putchar
+	mov a, #0x55
+	lcall Send_SPI
+	lcall putchar
+	mov a, #0x55
+	lcall Send_SPI
+	lcall putchar
+	setb FLASH_CE ; Disable SPI Flash
+	ljmp forever_loop	
+Command_0_skip:
+
+;---------------------------------------------------------	
+	cjne a, #'1' , Command_1_skip 
+Command_1_start: ; Erase whole flash (takes a long time)
+	lcall Enable_Write
+	clr FLASH_CE
+	mov a, #ERASE_ALL
+	lcall Send_SPI
+	setb FLASH_CE
+	lcall Check_WIP
+	mov a, #0x01 ; Send 'I am done' reply
+	lcall putchar		
+	ljmp forever_loop	
+Command_1_skip:
+
+;---------------------------------------------------------	
+	cjne a, #'2' , Command_2_skip 
+Command_2_start: ; Load flash page (256 bytes or less)
+	lcall Enable_Write
+	clr FLASH_CE
+	mov a, #WRITE_BYTES
+	lcall Send_SPI
+	lcall getchar ; Address bits 16 to 23
+	lcall Send_SPI
+	lcall getchar ; Address bits 8 to 15
+	lcall Send_SPI
+	lcall getchar ; Address bits 0 to 7
+	lcall Send_SPI
+	lcall getchar ; Number of bytes to write (0 means 256 bytes)
+	mov r0, a
+Command_2_loop:
+	lcall getchar
+	lcall Send_SPI
+	djnz r0, Command_2_loop
+	setb FLASH_CE
+	lcall Check_WIP
+	mov a, #0x01 ; Send 'I am done' reply
+	lcall putchar		
+	ljmp forever_loop	
+Command_2_skip:
+
+;---------------------------------------------------------	
+	cjne a, #'3' , Command_3_skip 
+Command_3_start: ; Read flash bytes (256 bytes or less)
+	clr FLASH_CE
+	mov a, #READ_BYTES
+	lcall Send_SPI
+	lcall getchar ; Address bits 16 to 23
+	lcall Send_SPI
+	lcall getchar ; Address bits 8 to 15
+	lcall Send_SPI
+	lcall getchar ; Address bits 0 to 7
+	lcall Send_SPI
+	lcall getchar ; Number of bytes to read and send back (0 means 256 bytes)
+	mov r0, a
+
+Command_3_loop:
+	mov a, #0x55
+	lcall Send_SPI
+	lcall putchar
+	djnz r0, Command_3_loop
+	setb FLASH_CE	
+	ljmp forever_loop	
+Command_3_skip:
+
+;---------------------------------------------------------	
+	cjne a, #'4' , Command_4_skip 
+Command_4_start: ; Playback a portion of the stored wav file
+	clr TR1 ; Stop Timer 1 ISR from playing previous request
+	setb FLASH_CE
+	nop
+	clr FLASH_CE ; Enable SPI Flash
+	mov a, #READ_BYTES
+	lcall Send_SPI
+	; Get the initial position in memory where to start playing
+	lcall getchar
+	lcall Send_SPI
+	lcall getchar
+	lcall Send_SPI
+	lcall getchar
+	lcall Send_SPI
+	; Get how many bytes to play
+	lcall getchar
+	mov w+2, a
+	lcall getchar
+	mov w+1, a
+	lcall getchar
+	mov w+0, a
+	
+	mov a, #0x00 ; Request first byte to send to DAC
+	lcall Send_SPI
+	
+	setb TR1 ; Start playback by enabling timer 1
+	ljmp forever_loop	
+Command_4_skip:
+
+;---------------------------------------------------------	
+	cjne a, #'5' , Command_5_skip 
+Command_5_start: ; Calculate and send CRC-16 of ISP flash memory from zero to the 24-bit passed value.
+	; Get how many bytes to use to calculate the CRC.  Store in [r5,r4,r3]
+	lcall getchar
+	mov r5, a
+	lcall getchar
+	mov r4, a
+	lcall getchar
+	mov r3, a
+	
+	; Since we are using the 'djnz' instruction to check, we need to add one to each byte of the counter.
+	; A side effect is that the down counter becomes efectively a 23-bit counter, but that is ok
+	; because the max size of the 25Q32 SPI flash memory is 400000H.
+	inc r3
+	inc r4
+	inc r5
+	
+	; Initial CRC must be zero.  Using [r7,r6] to store CRC.
+	clr a
+	mov r7, a
+	mov r6, a
+
+	clr FLASH_CE
+	mov a, #READ_BYTES
+	lcall Send_SPI
+	clr a ; Address bits 16 to 23
+	lcall Send_SPI
+	clr a ; Address bits 8 to 15
+	lcall Send_SPI
+	clr a ; Address bits 0 to 7
+	lcall Send_SPI
+	sjmp Command_5_loop_start
+
+Command_5_loop:
+	lcall Send_SPI
+	crc16() ; Calculate CRC with new byte
+Command_5_loop_start:
+	; Drecrement counter:
+	djnz r3, Command_5_loop
+	djnz r4, Command_5_loop
+	djnz r5, Command_5_loop
+	
+	setb FLASH_CE ; Done reading from SPI flash
+	
+	; Computation of CRC is complete.  Send 16-bit result using the serial port
+	mov a, r7
+	lcall putchar
+	mov a, r6
+	lcall putchar
+
+	ljmp forever_loop	
+Command_5_skip:
+
+;---------------------------------------------------------	
+	cjne a, #'6' , Command_6_skip 
+Command_6_start: ; Fill flash page (256 bytes)
+	lcall Enable_Write
+	clr FLASH_CE
+	mov a, #WRITE_BYTES
+	lcall Send_SPI
+	lcall getchar ; Address bits 16 to 23
+	lcall Send_SPI
+	lcall getchar ; Address bits 8 to 15
+	lcall Send_SPI
+	lcall getchar ; Address bits 0 to 7
+	lcall Send_SPI
+	lcall getchar ; Byte to write
+	mov r1, a
+	mov r0, #0 ; 256 bytes
+Command_6_loop:
+	mov a, r1
+	lcall Send_SPI
+	djnz r0, Command_6_loop
+	setb FLASH_CE
+	lcall Check_WIP
+	mov a, #0x01 ; Send 'I am done' reply
+	lcall putchar		
+	ljmp forever_loop	
+Command_6_skip:
+
+	ljmp forever_loop
+
 END
